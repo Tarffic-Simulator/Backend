39963308bfdc4e08f8e8c9c5336434fac891411a
AlairTox <alair.toxtega@gmail.com>
2026-05-28 16:58:28 -0600

    style: fix format check(ruff)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 
-
 os.environ["SECRET_KEY"] = "12345678901234567890123456789012"
 os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///:memory:"
 os.environ["ENGINE_API_URL"] = "http://localhost:8000"
